dc449c02616c5c71b1b5a0467543a10ff8de03e6
phobos927 <30345788+phobos927@users.noreply.github.com>
2017-10-21 12:03:22 +0700

    Условие по типам jpg png gif

--- HTML_table_generator/MainWindow.xaml.cs
+++ HTML_table_generator/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using WPFFolderBrowser;
+
 
 namespace HTML_table_generator
 {
@@ -20,9 +23,96 @@ namespace HTML_table_generator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = null;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            WPFFolderBrowserDialog open = new WPFFolderBrowserDialog();
+            bool sd = Convert.ToBoolean(open.ShowDialog());
+            if (sd)
+            {
+                path = open.FileName;
+            }
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (path != null)
+            {
+                string[] imgmass = Directory.GetFiles(path).Select(file => System.IO.Path.GetFileName(file)).ToArray();
+                string spath = textBoxP.Text;
+                int width = Convert.ToInt32(textBoxW.Text);
+                int height = Convert.ToInt32(textBoxH.Text);
+                int kol = Convert.ToInt32(textBoxС.Text);
+
+                int schet = 0;
+                int schet2 = 0;
+                using (StreamWriter outputFile = new StreamWriter("Code.txt"))
+                {
+                    foreach (string img in imgmass)
+                    {
+                        schet2++;
+                        if ((img.Substring(img.Length - 4) == ".jpg") || (img.Substring(img.Length - 4) == ".png") || (img.Substring(img.Length - 4) == ".gif"))
+                        {
+                            schet++;
+                           
+                            if (schet == 1)
+                            {
+                                outputFile.WriteLine("<tr>");
+                            }
+
+                            outputFile.Write("<td style=\"border-image: initial;\"><a rel=\"lightbox-cats\" href=\"");
+                            if (spath.Substring(0, 1) != "/")
+                            {
+                                outputFile.Write("/");
+
+                            }
+                            outputFile.Write(spath);
+                            if (spath.Substring(spath.Length - 1, 1) != "/")
+                            {
+                                outputFile.Write("/");
+                            }
+                            outputFile.Write(img);
+                            outputFile.Write("\" ><img width=\"");
+                            outputFile.Write(width);
+                            outputFile.Write("\" height=\"");
+                            outputFile.Write(height);
+                            outputFile.Write("\" src=\"");
+                            //
+                            if (spath.Substring(0, 1) != "/")
+                            {
+                                outputFile.Write("/");
+
+                            }
+                            outputFile.Write(spath);
+                            if (spath.Substring(spath.Length - 1, 1) != "/")
+                            {
+                                outputFile.Write("/");
+                            }
+                            //
+                            outputFile.Write(img);
+                            outputFile.WriteLine("\"  /></a></td>");
+                        }
+                            if (schet == kol || schet2 == imgmass.Length)
+                            {
+                                outputFile.WriteLine("</tr>");
+                                schet = 0;
+                            }
+                        
+                    }
+
+                        MessageBox.Show("Файл создан");
+                    
+                }
+            }
+            else { MessageBox.Show("Вы не выбрали папку"); }
+
+            }
+        }
     }
-}
+
